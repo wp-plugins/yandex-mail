7ea51f1d477bc5bfbf310cf557cd882d53c13806
GZep <GZep@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-16 19:59:14 +0000

    Mail testing functionality removed
    git-svn-id: https://plugins.svn.wordpress.org/yandex-mail/trunk@1200442 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- options.php
+++ options.php
@@ -10,9 +10,6 @@ class YandexMail
     public function __construct()
     {
         add_action('init', [$this, 'init']);
-        if (isset($_POST['action']) && $_POST['action'] == 'test') {
-            add_action('init', [$this, 'do_test']);
-        }
         add_action('admin_menu', [$this, 'admin_menu']);
         add_action('admin_init', [$this, 'admin_init']);
         if (!extension_loaded('openssl')) {
@@ -26,13 +23,6 @@ class YandexMail
         $this->options = get_option('yandex_mail');
     }
 
-    public function do_test()
-    {
-        check_admin_referer('test-options');
-        $result = wp_mail($_POST['to'], $_POST['subject'], $_POST['message']);
-        add_action('admin_notices', [$this, ($result ? 'test_ok_notice' : 'test_error_notice')]);
-    }
-
     public function admin_menu()
     {
         $title = __('Yandex Mail', self::DOMAIN); 
@@ -42,6 +32,7 @@ class YandexMail
     public function admin_init()
     {
         register_setting('yandex_mail', 'yandex_mail');
+        add_settings_section('default', false, false, 'yandex-mail');
         add_settings_field('from', __('From Name', self::DOMAIN), [$this, 'field_callback'],
             'yandex-mail', 'default', ['type' => 'text', 'name' => 'from']);
         add_settings_field('login', __('Login', self::DOMAIN), [$this, 'field_callback'],
@@ -58,26 +49,9 @@ class YandexMail
             . '<h2>' . __('Yandex Mail', self::DOMAIN) . '</h2>'
             . '<form method="post" action="options.php">';
         settings_fields('yandex_mail');
-        echo '<table class="form-table">';
-        do_settings_fields('yandex-mail', 'default');
-        echo '</table>';
+        do_settings_sections('yandex-mail');
         submit_button(); 
-        echo '</form>'
-            . '<h3>' . __('Test Email', self::DOMAIN) . '</h3>'
-            . '<form method="post">'
-            . '<input type="hidden" name="action" value="test" />';
-        wp_nonce_field('test-options');
-        echo '<table class="form-table">'
-            . '<tr><th scope="row">' . __('To', self::DOMAIN) . '</th>'
-            . '<td><input type="email" name="to" /></td></tr>'
-            . '<tr><th scope="row">' . __('Subject', self::DOMAIN) . '</th>'
-            . '<td><input type="text" name="subject" /></td></tr>'
-            . '<tr><th scope="row">' . __('Message', self::DOMAIN) . '</th>'
-            . '<td><textarea name="message" style="width: 100%; height: 5rem;"></textarea></td>'
-            . '</tr></table>'
-            . '<p class="submit">'
-            . '<input type="submit" class="button" value="' . __('Send Test Mail', self::DOMAIN) . '">'
-            . '</p></form></div>';
+        echo '</form></div>';
     }
 
     public function field_callback($args)
@@ -96,20 +70,6 @@ class YandexMail
     {
         echo '<div class="error"><p>'
             . __('The mod_ssl module is required', self::DOMAIN)
-            . '</p></div>';
-    }
-
-    public function test_ok_notice()
-    {
-        echo '<div class="updated"><p>'
-            . __('A test email has been successfully sent', self::DOMAIN)
-            . '</p></div>';
-    }
-
-    public function test_error_notice()
-    {
-        echo '<div class="error"><p>'
-            . __('A test email has not been sent', self::DOMAIN)
             . '</p></div>';
     }
 
